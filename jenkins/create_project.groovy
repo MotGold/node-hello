67f35c92c0c8961439b9cec1864cab30e3cc5af0
MotGold <72747315+MotGold@users.noreply.github.com>
2021-11-24 19:39:50 +0200

    Update create_project.groovy

--- jenkins/create_project.groovy
+++ jenkins/create_project.groovy
@@ -1,20 +1,20 @@
 job('NodeJS Docker example') {
     scm {
-        git('git://github.com/nlitterat/node-hello.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('nlitterat')
-            node / gitConfigEmail('nlitterat@gmail.com')
+        git('git://github.com/MotGold/node-hello.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('MotGold')
+            node / gitConfigEmail('motgold@walla.co.il')
         }
     }
     triggers {
         scm('H/5 * * * *')
     }
     wrappers {
-        nodejs('Node 17.0.1') // this is the name of the NodeJS installation in 
+        nodejs('Node 17.1.0') // this is the name of the NodeJS installation in 
                          // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('nlitterat/compie')
+            repositoryName('motgold/compie-jenkins-1')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
